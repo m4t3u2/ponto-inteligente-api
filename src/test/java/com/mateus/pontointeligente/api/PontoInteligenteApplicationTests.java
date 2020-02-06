192134b33a6cdd1caf48d867adb8a086c2bfa7a1
m4t3u2 <ms.mateus@hotmail.com>
2020-02-06 00:39:33 -0300

    Remoção JUnit Jupiter.

--- src/test/java/com/mateus/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/mateus/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mateus.pontointeligente.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-class PontoInteligenteApplicationTests {
+public class PontoInteligenteApplicationTests {
 
 	@Test
 	void contextLoads() {
