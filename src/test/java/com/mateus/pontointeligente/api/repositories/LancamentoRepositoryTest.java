b05dbc3e716a39d7f74a72213133d5767b7e8f67
m4t3u2 <ms.mateus@hotmail.com>
2020-02-11 21:03:52 -0300

    Serviços e Testes.

--- src/test/java/com/mateus/pontointeligente/api/repositories/LancamentoRepositoryTest.java
+++ src/test/java/com/mateus/pontointeligente/api/repositories/LancamentoRepositoryTest.java
@@ -6,8 +6,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.List;
 
-import javax.print.attribute.standard.PageRanges;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -68,8 +64,7 @@ public class LancamentoRepositoryTest {
 	
 	@Test
 	public void testBuscarLancamentosPorFuncionarioIdPaginado() {
-		PageRequest page = null;
-		//PageRequest page = new PageRequest(0, 10);
+		PageRequest page = PageRequest.of(0, 10);
 		Page<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId, page);
 		
 		assertEquals(2, lancamentos.getTotalElements());
