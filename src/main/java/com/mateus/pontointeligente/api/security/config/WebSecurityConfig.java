1df716abbd534ca3835605b0de249aa865766d69
Mateus Simon <ms.mateus@hotmail.com>
2022-08-10 12:08:08 -0300

    Desabilitado Spring Security.

---
+++ src/main/java/com/mateus/pontointeligente/api/security/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.mateus.pontointeligente.api.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated().and().csrf()
+				.disable();
+	}
+
+}
